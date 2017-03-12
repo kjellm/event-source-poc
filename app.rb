8aacdcc82ec79017c0cd7d63af06bdd0382753b8
Kjell-Magne Øierud <kjellm@oierud.net>
2017-03-12 09:43:22 +0100

    Refactor app + add some examples of rebuilding projections

--- app.rb
+++ app.rb
@@ -3,34 +3,22 @@ require_relative 'event'
 require_relative 'cmd'
 require_relative 'crud'
 require_relative 'model'
+require_relative 'read'
 
 require 'pp'
 
 class Application < BaseObject
 
+  def initialize
+    @recording_id = UUID.generate
+    @release_id = UUID.generate
+    @checkpoint = Time.now
+    initialize_projections
+  end
+
   def main
-    puts "LOGG ---------------------------------------------------------"
-    recording_id = UUID.generate
-    recording_data = {id: recording_id, title: "Sledge Hammer",
-                      artist: "Peter Gabriel", duration: 313}
-    run(recording_data, CreateRecording, Recording)
-
-    release_id = UUID.generate
-    run({id: release_id, title: "So", tracks: []},
-        CreateRelease, Release)
-    run({id: UUID.generate, title: "Shaking The Tree",
-         tracks: [recording_id]},
-        CreateRelease, Release)
-
-    run({id: release_id, title: "So", tracks: [recording_id]},
-        UpdateRelease, Release)
-
-    run(recording_data.merge({ title:  "Sledgehammer" }),
-        UpdateRecording, Recording)
-
-    # Some failing commands, look in log for verification of failure
-    run({id: "Non-existing ID", title: "Foobar"},
-        UpdateRecording, Recording)
+    puts "AUDIT LOGG -------------------------------------------------"
+    run_commands
 
     puts
     puts "EVENT STORE ------------------------------------------------"
@@ -40,24 +28,71 @@ class Application < BaseObject
     puts "EVENT LOGG -------------------------------------------------"
     pp TheEventLogg
 
-
     puts
     puts "PROJECTIONS ------------------------------------------------"
-    # FIXME lock event store
-    require_relative 'read'
-    puber = EventPublisher.new()
-    projections = [TheReleaseProjection, TheRecordingProjection, TheTotalsProjection]
-    projections.each do |pr|
-      puber.add_subscriber(pr)
-    end
-    puber.publish(*TheEventLogg.to_a)
-
-    p TheReleaseProjection.find release_id
-    p TheRecordingProjection.find recording_id
-    p TheTotalsProjection.totals
+    print_projections
+    rebuild_projections_from(TheEventLogg.upto(@checkpoint))
+    puts
+    puts "Upto #@checkpoint"
+    print_projections
+    rebuild_projections_from(TheEventLogg.to_a)
+    puts
+    puts "All"
+    print_projections
   end
 
   private
+
+  def initialize_projections
+    @the_recording_projection = RecordingProjection.new
+    @the_release_projection = ReleaseProjection.new(@the_recording_projection)
+    @the_totals_projection = TotalsProjection.new
+
+    @projections = [
+      @the_release_projection,
+      @the_recording_projection,
+      @the_totals_projection,
+    ]
+  end
+
+  def rebuild_projections_from(events)
+    # FIXME lock event store
+    initialize_projections
+
+    puber = EventPublisher.new()
+    @projections.each {|pr| puber.add_subscriber(pr) }
+    puber.publish(*events)
+  end
+
+  def print_projections
+    p @the_release_projection.find @release_id
+    p @the_recording_projection.find @recording_id
+    p @the_totals_projection.totals
+  end
+
+  def run_commands
+    recording_data = {id: @recording_id, title: "Sledge Hammer",
+                      artist: "Peter Gabriel", duration: 313}
+    run(recording_data, CreateRecording, Recording)
+
+    run({id: @release_id, title: "So", tracks: []},
+        CreateRelease, Release)
+    run({id: UUID.generate, title: "Shaking The Tree",
+         tracks: [@recording_id]},
+        CreateRelease, Release)
+
+    run({id: @release_id, title: "So", tracks: [@recording_id]},
+        UpdateRelease, Release)
+
+    @checkpoint = Time.now
+
+    run(recording_data.merge({ title:  "Sledgehammer" }),
+        UpdateRecording, Recording)
+
+    # Some failing commands, look in log for verification of failure
+    run({id: "Non-existing ID", title: "Foobar"},
+        UpdateRecording, Recording)
+  end
 
   def run(request_data, command_class, aggregate)
     logg "Incoming request with data: #{request_data.inspect}"
