kjellm/event-source-poc

commit 8bc5116493cd4491f327110250c5585075e99910

Add command router

app.rb
@@ -16,7 +16,7 @@ class Application < BaseObject
   end
 
   def main
-    puts "LOGG ---------------------------------------------------------"
+    puts "LOGG -------------------------------------------------------"
     run_commands
 
     puts
@@ -49,32 +49,25 @@ class Application < BaseObject
   end
 
   def run_commands
-    recording_data = {id: @recording_id, title: "Sledge Hammer",
-                      artist: "Peter Gabriel", duration: 313}
-    run(recording_data, CreateRecording, Recording)
+    run CreateRecording, id: @recording_id, title: "Sledge Hammer",
+                         artist: "Peter Gabriel", duration: 313
 
-    run({id: @release_id, title: "So", tracks: []},
-        CreateRelease, Release)
-    run({id: UUID.generate, title: "Shaking The Tree",
-         tracks: [@recording_id]},
-        CreateRelease, Release)
+    run CreateRelease, id: @release_id, title: "So", tracks: []
+    run CreateRelease, id: UUID.generate, title: "Shaking The Tree",
+                       tracks: [@recording_id]
 
-    run({id: @release_id, title: "So", tracks: [@recording_id]},
-        UpdateRelease, Release)
+    run UpdateRelease, id: @release_id, title: "So", tracks: [@recording_id]
 
-    run(recording_data.merge({ title:  "Sledgehammer" }),
-        UpdateRecording, Recording)
+    run UpdateRecording, title: "Sledgehammer"
 
     # Some failing commands, look in log for verification of failure
-    run({id: "Non-existing ID", title: "Foobar"},
-        UpdateRecording, Recording)
+    run UpdateRecording, id: "Non-existing ID", title: "Foobar"
   end
 
-  def run(request_data, command_class, aggregate)
-    logg "Incoming request with data: #{request_data.inspect}"
-    command_handler = registry.command_handler_for(aggregate)
-    command = command_class.new(request_data)
-    command_handler.handle command
+  def run(command_class, data)
+    logg "Incoming #{command_class.name} request with data: #{data}"
+    command = command_class.new data
+    registry.command_router.route(command)
   rescue StandardError => e
     logg "ERROR: Command #{command} failed because of: #{e}"
   end

base.rb
@@ -93,13 +93,8 @@ end
 
 class Registry < BaseObject
 
-  def command_handler_for(klass)
-    handler = if klass.respond_to? :handle
-                klass
-              else
-                self.class.const_get("#{klass}CommandHandler").new
-              end
-    CommandHandlerLoggDecorator.new(handler)
+  def command_router
+    @command_router ||= CommandRouter.new
   end
 
   def event_store

cmd.rb
@@ -34,6 +34,30 @@ class Command < ValueObject
 
 end
 
+class CommandRouter < BaseObject
+
+  def initialize
+    @handlers = {}
+  end
+
+  def register_handler(handler, *command_classes)
+    command_classes.each do |cmd|
+      @handlers[cmd] = CommandHandlerLoggDecorator.new(handler)
+    end
+  end
+
+  def route(command)
+    handler_for(command).public_send :handle, command
+  end
+
+  private
+
+  def handler_for(command)
+    @handlers.fetch command.class
+  end
+
+end
+
 class CommandHandler < BaseObject
 
   module InstanceMethods

model.rb
@@ -10,16 +10,6 @@
 
 RELEASE_ATTRIBUTES = %I(id title tracks)
 
-class Release < Entity
-  attributes *RELEASE_ATTRIBUTES
-
-  include CrudAggregate
-
-  def assert_validity
-    # Do something here
-  end
-end
-
 class ReleaseCommand < Command
 
   private
@@ -46,57 +36,24 @@ class ReleaseUpdated < Event
   attributes *RELEASE_ATTRIBUTES
 end
 
-#
-# R E C O R D I N G
-#
-# Shows an example where all the different responsibilities are
-# handled by separate objects.
-#
+class Release < Entity
+  attributes *RELEASE_ATTRIBUTES
 
-class RecordingRepository < EventStoreRepository
+  registry.command_router.register_handler(self, CreateRelease, UpdateRelease)
 
-  def type
-    Recording
-  end
-
-  def apply_recording_updated(recording, event)
-    recording.set_attributes(event.to_h)
-  end
-
-end
-
-class RecordingValidator < BaseObject
-
-  def initialize(obj)
-  end
+  include CrudAggregate
 
   def assert_validity
     # Do something here
   end
 end
 
-class RecordingCommandHandler < CrudCommandHandler
-
-  private
-
-  def type; Recording; end
-
-  def repository
-    @repository ||= registry.repository_for(Recording)
-  end
-
-  def validator(obj)
-    RecordingValidator.new(obj)
-  end
-
-  def process_create_recording(command)
-    process_create(command)
-  end
-
-  def process_update_recording(command)
-    process_update(command)
-  end
-end
+#
+# R E C O R D I N G
+#
+# Shows an example where all the different responsibilities are
+# handled by separate objects.
+#
 
 RECORDING_ATTRIBUTES = %I(id title artist duration)
 
@@ -126,6 +83,53 @@ end
 
 class RecordingUpdated < Event
   attributes *RECORDING_ATTRIBUTES
+end
+
+class RecordingRepository < EventStoreRepository
+
+  def type
+    Recording
+  end
+
+  def apply_recording_updated(recording, event)
+    recording.set_attributes(event.to_h)
+  end
+
+end
+
+class RecordingValidator < BaseObject
+
+  def initialize(obj)
+  end
+
+  def assert_validity
+    # Do something here
+  end
+end
+
+class RecordingCommandHandler < CrudCommandHandler
+
+  registry.command_router.register_handler(self.new, CreateRecording, UpdateRecording)
+
+  private
+
+  def type; Recording; end
+
+  def repository
+    @repository ||= registry.repository_for(Recording)
+  end
+
+  def validator(obj)
+    RecordingValidator.new(obj)
+  end
+
+  def process_create_recording(command)
+    process_create(command)
+  end
+
+  def process_update_recording(command)
+    process_update(command)
+  end
 end
 
 class Recording < Entity

commit 534a3868871cecdf2c59e73d59e4a0d1dd2f2c24

Fix regression in app.rb, introduced in 8bc5116

app.rb
@@ -74,8 +74,13 @@ class Application < BaseObject
   end
 
   def run_commands
-    run CreateRecording, id: @recording_id, title: "Sledge Hammer",
-                         artist: "Peter Gabriel", duration: 313
+    sledgehammer = {
+      id: @recording_id,
+      title: "Sledge Hammer",
+      artist: "Peter Gabriel",
+      duration: 313,
+    }
+    run CreateRecording, sledgehammer
 
     run CreateRelease, id: @release_id, title: "So", tracks: []
     run CreateRelease, id: UUID.generate, title: "Shaking The Tree",
@@ -85,7 +90,7 @@ class Application < BaseObject
 
     @checkpoint = Time.now
 
-    run UpdateRecording, title: "Sledgehammer"
+    run UpdateRecording, sledgehammer.merge({title: "Sledgehammer"})
 
     # Some failing commands, look in log for verification of failure
     run UpdateRecording, id: "Non-existing ID", title: "Foobar"